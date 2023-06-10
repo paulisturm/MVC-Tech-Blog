611697defabb0dc5384e8254c0130bb43baea721
paulisturm <paulisturm22@gmail.com>
2023-06-10 12:17:36 -0400

    handlebars:

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exphbs = require("express-handlebars")
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//used for helpers in utils folder for formatting date
+const hbs = exphbs.create({helpers})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +21,10 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
